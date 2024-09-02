a70b31beb229323b67cedf10bb5c1ca4b96b89af
MosabXR <117486800+MosabXR@users.noreply.github.com>
2024-09-02 20:11:33 +0300

    Update WishlistAPI.js

--- src/Components/APIs/WishlistAPI.js
+++ src/Components/APIs/WishlistAPI.js
@@ -1,18 +1,17 @@
 import baseURL from "./BaseURL";
-import token from "./UserToken";
 import axios from "axios";
 
-const getUserWishlistAPI = async () => {
+const getUserWishlistAPI = async (token) => {
     const { data } = await axios.get(`${baseURL}/wishlist`, { headers: { token } });
     return data;
 }
 
-const addProductToWishlistAPI = async productId => {
+const addProductToWishlistAPI = async (productId,token) => {
     const { data } = await axios.post(`${baseURL}/wishlist`, { productId }, { headers: { token } });
     return data;
 }
 
-const removeProductFromWishlistAPI = async productId => {
+const removeProductFromWishlistAPI = async (productId,token) => {
     const { data } = await axios.delete(`${baseURL}/wishlist/${productId}`, { headers: { token } });
     return data;
 }
